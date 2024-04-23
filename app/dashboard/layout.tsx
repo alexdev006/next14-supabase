7d97ccf08231dd0c52e013e47280cbada03eb08d
Alex <alexdev0605@gmail.com>
2024-04-23 14:25:49 +0200

    feat: setup prisma + redirect route if not loggin & redirect to dashboard if login

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,20 @@
-import DashboardNav from "@/components/dashboardNav";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function DashboardLayout({
+import DashboardNav from "@/components/dashboardNav";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    return redirect("/");
+  }
+
   return (
     <div className="flex flex-col space-y-6 mt-10">
       <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
