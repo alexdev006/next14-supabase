dba29b843c50ad9f9c76f24be69943f8c4d5616f
Alex <alexdev0605@gmail.com>
2024-04-23 11:53:02 +0200

    feat: add kinde auth login log out

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Hello </p>
-    </main>
+    <section className="flex items-center justify-center bg-background h-[90vh]">
+      <div className="relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
+        <div className="max-w-3xl mx-auto text-center">
+          <span className="w-auto px-6 py-3 rounded-full bg-secondary">
+            <span className="text-sm font-medium text-primary">
+              Sort yours notes easily
+            </span>
+          </span>
+        </div>
+      </div>
+    </section>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,34 @@
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
 import { ThemeToggle } from "./theme-toggle";
 import { Button } from "./ui/button";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const { isAuthenticated } = getKindeServerSession();
   return (
     <div className="border border-b-2 p-2 flex justify-between h-[5vh] items-center">
       <div className="">Logo</div>
       <div className="">Nav</div>
       <div className="flex gap-x-2">
-        <Button>Sign in</Button>
-        <Button variant="secondary">Sign up</Button>
+        {(await isAuthenticated()) ? (
+          <LogoutLink>
+            <Button>Log out</Button>
+          </LogoutLink>
+        ) : (
+          <>
+            <LoginLink>
+              <Button>Sign in</Button>
+            </LoginLink>
+            <RegisterLink>
+              <Button variant="secondary">Sign up</Button>
+            </RegisterLink>
+          </>
+        )}
         <ThemeToggle />
       </div>
     </div>
