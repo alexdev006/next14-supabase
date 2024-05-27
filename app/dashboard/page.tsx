8f4d53418e299ba9f4242226844a5338b852db66
Alex <alexdev0605@gmail.com>
2024-05-27 13:48:41 +0200

    feat: list note

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
-import prisma from "@/app/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Edit, File, Trash } from "lucide-react";
 
+import prisma from "@/app/lib/db";
 import { Button } from "@/components/ui/button";
-import { File } from "lucide-react";
+import { Card } from "@/components/ui/card";
 
 async function getData(userId: string) {
   const data = await prisma.note.findMany({
@@ -52,7 +53,37 @@ export default async function DashboardPage() {
           </Button>
         </div>
       ) : (
-        <div></div>
+        <div className="flex flex-col gap-y-4">
+          {data.map((item) => (
+            <Card
+              key={item.id}
+              className="flex items-center justify-between p-4"
+            >
+              <div className="">
+                <h2 className="font-semibold text-xl text-primary">
+                  {item.title}
+                </h2>
+                <p>
+                  {new Intl.DateTimeFormat("fr-FR", {
+                    dateStyle: "full",
+                  }).format(new Date(item.createdAt))}
+                </p>
+              </div>
+              <div className="flex gap-x-4">
+                <Link href={`/dashboard/new/${item.id}`}>
+                  <Button variant="outline" size="icon">
+                    <Edit className="w-4 h-4" />
+                  </Button>
+                </Link>
+                <form>
+                  <Button variant="destructive" size="icon">
+                    <Trash className="w-4 h-4" />
+                  </Button>
+                </form>
+              </div>
+            </Card>
+          ))}
+        </div>
       )}
     </div>
   );
